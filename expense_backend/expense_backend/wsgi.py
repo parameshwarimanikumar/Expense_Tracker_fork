197f9c4a78657c2566de130ae165983425aec16d
spara <parameshwarimanikumar@gmail.com>
2025-07-10 16:49:39 +0530

    Temp: auto-create superuser on WSGI startup

--- expense_backend/expense_backend/wsgi.py
+++ expense_backend/expense_backend/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for expense_backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
+# Add this only temporarily to auto-create a superuser
 import os
-
+from django.contrib.auth import get_user_model
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expense_backend.settings')
-
 application = get_wsgi_application()
+
+User = get_user_model()
+email = "paramesh@email.com"
+password = "admin123"
+
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(email=email, password=password)
