3ab457f4112287b37ed7128993451b3ecb7737f6
spara <parameshwarimanikumar@gmail.com>
2025-07-10 15:39:07 +0530

    Temporarily promote user to superuser on startup

--- expense_backend/expense_app/apps.py
+++ expense_backend/expense_app/apps.py
@@ -1,9 +1,10 @@
+# expense_app/apps.py
 from django.apps import AppConfig
-
 
 class ExpenseAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'expense_app'
-    
+
     def ready(self):
-        import expense_app.signals
+        from . import promote_admin
+        promote_admin.run()
