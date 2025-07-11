d2362e37200e05b821ee42ddbddc2864d808dcf9
spara <parameshwarimanikumar@gmail.com>
2025-07-11 10:50:12 +0530

    Update frontend build or environment

--- expense_app_frontend/src/api_service/api.js
+++ expense_app_frontend/src/api_service/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // Set up Axios instance
-const BASE_URL = 'http://127.0.0.1:8000/api/';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api/';
+
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
